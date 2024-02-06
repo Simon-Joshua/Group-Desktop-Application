6c900947ce2c68cbe80d3e041414fa93917d0f39
Simon-Joshua <158138258+Simon-Joshua@users.noreply.github.com>
2024-02-06 10:24:03 +0100

    Update Desktop_application.py

--- Desktop_application.py
+++ Desktop_application.py
@@ -23,38 +23,3 @@ french_dict = {
     "code": "code",
     "language": "langue",
     "programming": "programmation",
-    "software": "logiciel",
-    "internet": "internet",
-    "data": "données",
-    "science": "science",
-    "machine": "machine",
-    "learning": "apprentissage",
-    "artificial": "artificielle",
-    "intelligence": "intelligence",
-    "developer": "développeur",
-    "keyboard": "clavier",
-    "mouse": "souris",
-    "screen": "écran",
-    "algorithm": "algorithme",
-    "database": "base de données",
-    "function": "fonction",
-    "variable": "variable",
-    "loop": "boucle",
-    "conditional": "conditionnelle",
-    "statement": "déclaration",
-    "debugging": "débogage",
-    "comment": "commentaire"
-
-}
-def search(word):
-    if word in french_dict:
-        result.set("french_dict[word]")
-        print(french_dict[word])
-    else:
-        result.set("Not Found")
-        print("Not Found")
-search_btn = Button(window, text="search", command=lambda :search(entry_text.get()))
-search_btn.pack()
-result_Label.pack()
-window.mainloop()
-  
